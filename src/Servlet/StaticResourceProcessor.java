69955208aed965d5e28fe3c069ba82256b81cb21
cyclone17 <thycyclone@163.com>
2016-10-15 17:47:44 +0800

    Add files via upload

---
+++ src/Servlet/StaticResourceProcessor.java
@@ -0,0 +1,19 @@
+package Servlet;
+
+
+import java.io.IOException;
+
+public class StaticResourceProcessor
+{
+	public void process(Request request, Response response)
+	{
+		try
+		{
+			response.sendStaticResource();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
